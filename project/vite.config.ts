6ce2cfdbcf2395b6d44976c9505aac8f7f97ce7b
Nouamane Tazi <nouamane98@gmail.com>
2025-04-22 00:44:12 +0200

    .

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+    global: {},
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      zlib: 'browserify-zlib',
+      util: 'util',
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
